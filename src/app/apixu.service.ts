59b2d2a67c3164c8af9881eebb5c848837b9691e
vronney <ronneyvargas@gmail.com>
2020-06-17 16:25:52 -0500

    Added new api

--- src/app/apixu.service.ts
+++ src/app/apixu.service.ts
@@ -9,8 +9,11 @@ export class ApixuService {
   constructor(private http: HttpClient) { }
 
   getWeather(location) {
+    // return this.http.get(
+    //   'http://api.weatherstack.com/current?access_key=de502a1cf3d0ed2534f3065b7e9a669d&query=' + location + '&units=f'
+    // );
     return this.http.get(
-      'http://api.weatherstack.com/current?access_key=de502a1cf3d0ed2534f3065b7e9a669d&query=' + location + '&units=f'
+      'https://api.weatherbit.io/v2.0/current?city= ' + location + '&key=bb28421491794a85978d17ba66dab077&lang=en&units=I'
     );
   }
 
