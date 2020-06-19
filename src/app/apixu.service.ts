afc27f90efe09600f002a377952179db8245c70f
vronney <ronneyvargas@gmail.com>
2020-06-19 12:20:23 -0500

    Added if/else statement to check if location entered is a number of string.  Added bottom margin.

--- src/app/apixu.service.ts
+++ src/app/apixu.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { isNumber } from 'util';
 
 @Injectable({
   providedIn: 'root'
@@ -9,12 +10,18 @@ export class ApixuService {
   constructor(private http: HttpClient) { }
 
   getWeather(location) {
-    // return this.http.get(
-    //   'http://api.weatherstack.com/current?access_key=de502a1cf3d0ed2534f3065b7e9a669d&query=' + location + '&units=f'
-    // );
-    return this.http.get(
-      'https://api.weatherbit.io/v2.0/current?city= ' + location + '&key=bb28421491794a85978d17ba66dab077&lang=en&units=I'
-    );
+    const numbersOnly = /^[0-9]/;
+    // Will check if location values are numbers or a string
+    // User can input postal code or name of city and state
+    if (location.match(numbersOnly)) {
+      return this.http.get(
+        'https://api.weatherbit.io/v2.0/current?postal_code= ' + location + '&key=bb28421491794a85978d17ba66dab077&lang=en&units=I'
+      );
+    } else {
+      return this.http.get(
+        'https://api.weatherbit.io/v2.0/current?city= ' + location + '&key=bb28421491794a85978d17ba66dab077&lang=en&units=I'
+      );
+    }
   }
 
 }
